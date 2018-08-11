42e9360616484cfd8d6e41fad490aaebe051d9a1
Parkkkkk <mb7878@naver.com>
2018-08-11 21:04:24 +0900

    .

--- 4장/3.https,http2/server1-2.js
+++ 4장/3.https,http2/server1-2.js
@@ -1,14 +1,15 @@
-const https = require('https');
+// http2를 적용
+const http2 = require('http2');
 const fs = require('fs');
 
-https.createServer({
+http2.createSecureServer({
     cert : fs.readFileSync('도메인 인증서 경로'),
     key : fs.readFileSync('도메인 비밀키 경로'),
     ca : [
         fs.readFileSync('상위 인증서 경로'),
         fs.readFileSync('상위 인증서 경로'),
     ],
-}, (req,res) => {
+},(req,res)=> {
     res.write('<h1>Hello Node!</h1>');
     res.end('<p>Hello Server!</p>');
 }).listen(443, () => {
